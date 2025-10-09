03f23227542bfdec73088b1ea39b5d961471ae71
Anton Siligan <anton.siligan@neinja.dev>
2025-10-09 15:22:31 +0200

    add select wrapper

---
+++ src/components/simple-select.tsx
@@ -0,0 +1,27 @@
+import { LucideIcon } from 'lucide-react';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
+
+type SimpleSelectProps = {
+	value: string | undefined;
+	onChange: (value: string | undefined) => void;
+	options: { label: string; value: string; icon?: LucideIcon }[];
+	placeholder: string;
+};
+
+export const SimpleSelect = ({ value, onChange, options, placeholder }: SimpleSelectProps) => {
+	return (
+		<Select value={value} onValueChange={onChange}>
+			<SelectTrigger>
+				<SelectValue placeholder={placeholder} />
+			</SelectTrigger>
+			<SelectContent>
+				{options.map((option) => (
+					<SelectItem key={option.value} value={option.value}>
+						{option.icon && <option.icon className='size-4' />}
+						{option.label}
+					</SelectItem>
+				))}
+			</SelectContent>
+		</Select>
+	);
+};
