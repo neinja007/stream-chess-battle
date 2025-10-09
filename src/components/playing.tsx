204ea33da22015ec78972a5b7680ace430653140
Anton Siligan <anton.siligan@neinja.dev>
2025-10-09 16:03:40 +0200

    add basic playing page

--- src/components/playing.tsx
+++ src/components/playing.tsx
@@ -1,4 +1,6 @@
 import { Settings as SettingsType } from '@/types/settings';
+import { useState } from 'react';
+import { Chessboard } from './chessboard';
 
 type PlayingProps = {
 	settings: SettingsType;
@@ -6,5 +8,13 @@ type PlayingProps = {
 };
 
 export const Playing = ({ settings, setStatus }: PlayingProps) => {
-	return <div>Playing</div>;
+	const [game, setGame] = useState<string>('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
+
+	return (
+		<div className='w-full max-w-7xl mx-auto pt-24 flex flex-col gap-4'>
+			<div className='max-w-lg rounded-xl overflow-hidden'>
+				<Chessboard game={game} />
+			</div>
+		</div>
+	);
 };
