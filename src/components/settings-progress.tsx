7463f9a7d08f21d77ade73a638def829e149e571
Anton Siligan <anton.siligan@neinja.dev>
2025-10-09 15:15:12 +0200

    add settings components

---
+++ src/components/settings-progress.tsx
@@ -0,0 +1,22 @@
+import { Settings } from '@/types/settings';
+import { Progress } from './ui/progress';
+
+type SettingsProgressProps = {
+	settings: Settings;
+};
+
+export const SettingsProgress = ({ settings }: SettingsProgressProps) => {
+	const settingsCompletion: boolean[] = [
+		settings.moveSelection !== undefined,
+		settings.playerWhite.platform !== undefined,
+		settings.playerBlack.platform !== undefined,
+		settings.playerWhite.channel !== '',
+		settings.playerBlack.channel !== '',
+		settings.secondsPerMove !== 0,
+		settings.evaluationBar !== undefined
+	];
+
+	const completion = settingsCompletion.filter(Boolean).length / settingsCompletion.length;
+
+	return <Progress value={completion * 100} className='mt-2' />;
+};
---
+++ src/components/settings-section.tsx
@@ -0,0 +1,19 @@
+import { CircleCheck } from 'lucide-react';
+
+type SettingsSectionProps = {
+	title: string;
+	children: React.ReactNode;
+	completed: boolean;
+};
+
+export const SettingsSection = ({ title, children, completed }: SettingsSectionProps) => {
+	return (
+		<div className='flex flex-col gap-2 mt-3'>
+			<div className='flex justify-between items-center'>
+				<h2 className='text-xl'>{title}</h2>
+				{completed && <CircleCheck className='size-4 text-green-500' />}
+			</div>
+			{children}
+		</div>
+	);
+};
