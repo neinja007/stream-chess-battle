863017f71450032b1f3b4ab5743bb52adc268e3c
Anton Siligan <anton.siligan@neinja.dev>
2025-10-11 13:34:34 +0200

    use stockfish online

--- src/lib/get-evaluation.ts
+++ src/lib/get-evaluation.ts
@@ -1,7 +1,7 @@
 import { Evaluation } from '@/types/evaluation';
 
 export const getEvaluation = async (fen: string): Promise<Evaluation> => {
-	const response = await fetch(`https://lichess.org/api/cloud-eval?fen=${fen}&multiPv=0&variant=standard`);
+	const response = await fetch(`https://stockfish.online/api/s/v2.php?fen=${fen}&depth=15`);
 	const data = await response.json();
 	return { depth: data.depth, evaluation: data.pvs[0].cp || data.pvs[0].mate };
 };
