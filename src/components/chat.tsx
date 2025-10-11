0a12fada5c05fb1d65311e3e71e573c8e8171aee
Anton Siligan <anton.siligan@neinja.dev>
2025-10-11 11:56:41 +0200

    fix time display

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -10,9 +10,11 @@ type ChatProps = {
 	info: PlayerInfo;
 	color: 'white' | 'black';
 	clearMove: (move: string) => void;
+	timeLeft: number;
+	defaultTimeLeft: number;
 };
 
-export const Chat = ({ moves, info, color, clearMove }: ChatProps) => {
+export const Chat = ({ moves, info, color, clearMove, activeTurn, timeLeft, defaultTimeLeft }: ChatProps) => {
 	return (
 		<div className='flex flex-col gap-4 h-full overflow-hidden'>
 			<h1 className='text-lg font-semibold shrink-0 flex items-center justify-between gap-2'>
@@ -29,20 +31,32 @@ export const Chat = ({ moves, info, color, clearMove }: ChatProps) => {
 				</div>
 			</h1>
 			<div className='flex flex-col gap-2 overflow-y-auto flex-1 scroll-smooth'>
-				{moves
-					.sort((a, b) => b.count - a.count)
-					.slice(0, 5)
-					.map((move) => (
-						<div key={move.move} className='group text-sm flex items-center justify-between gap-2'>
-							<span>{move.move}</span>
-							<div>
-								<span className='visible group-hover:hidden text-muted-foreground'>{move.count}</span>
-								<button className='hidden group-hover:block' onClick={() => clearMove(move.move)}>
-									<X className='size-4' />
-								</button>
+				{activeTurn ? (
+					moves
+						.sort((a, b) => b.count - a.count)
+						.slice(0, 5)
+						.map((move) => (
+							<div key={move.move} className='group text-sm flex items-center justify-between gap-2'>
+								<span>{move.move}</span>
+								<div>
+									<span className='visible group-hover:hidden text-muted-foreground'>{move.count}</span>
+									<button className='hidden group-hover:block' onClick={() => clearMove(move.move)}>
+										<X className='size-4' />
+									</button>
+								</div>
 							</div>
-						</div>
-					))}
+						))
+				) : (
+					<div className='text-center text-muted-foreground'>Await your turn...</div>
+				)}
+			</div>
+			<div className='flex justify-between'>
+				<span>{moves.length} Votes</span>
+				<div
+					className={cn('rounded-md text-black px-2 py-0.5', activeTurn ? 'bg-red-500 animate-pulse' : 'bg-gray-500')}
+				>
+					{activeTurn ? timeLeft.toFixed(2) : defaultTimeLeft.toFixed(2)}
+				</div>
 			</div>
 		</div>
 	);
