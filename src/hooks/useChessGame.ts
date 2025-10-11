75e63e899f9939dcc33c3ca83e751a8d5d8c9ac9
Anton Siligan <anton.siligan@neinja.dev>
2025-10-11 12:18:14 +0200

    fix game rerender issues

--- src/hooks/useChessGame.ts
+++ src/hooks/useChessGame.ts
@@ -1,14 +1,42 @@
 import { Chess } from 'chess.js';
+import { useState, useCallback } from 'react';
 
 export const useChessGame = () => {
-	const game = new Chess();
+	const [game] = useState(() => new Chess());
+	const [position, setPosition] = useState(game.fen());
+	const [gameOver, setGameOver] = useState(game.isGameOver());
+	const [legalMoves, setLegalMoves] = useState(game.moves());
+	const [turn, setTurn] = useState(game.turn());
+
+	const updateGameState = useCallback(() => {
+		setPosition(game.fen());
+		setGameOver(game.isGameOver());
+		setLegalMoves(game.moves());
+		setTurn(game.turn());
+	}, [game]);
+
+	const move = useCallback(
+		(move: string) => {
+			const result = game.move(move);
+			if (result) {
+				updateGameState();
+			}
+			return result;
+		},
+		[game, updateGameState]
+	);
+
+	const reset = useCallback(() => {
+		game.reset();
+		updateGameState();
+	}, [game, updateGameState]);
 
 	return {
-		position: game.fen(),
-		move: game.move,
-		gameOver: game.isGameOver(),
-		legalMoves: game.moves(),
-		reset: game.reset,
-		turn: game.turn()
+		position,
+		move,
+		gameOver,
+		legalMoves,
+		reset,
+		turn
 	};
 };
