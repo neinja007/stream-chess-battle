6b49ffc04c72f75bf48d63f7d7ae1235fab4715e
Anton Siligan <anton.siligan@neinja.dev>
2025-10-11 11:33:31 +0200

    add process move util

---
+++ src/lib/process-move.ts
@@ -0,0 +1,28 @@
+import { Move } from '@/hooks/useChat';
+import { Dispatch, SetStateAction } from 'react';
+
+export const processMove = (
+	testAndTransformMove: (move: string) => string | undefined,
+	rawMove: { text: string; user: string },
+	moves: Move[],
+	setMoves: Dispatch<SetStateAction<Move[]>>
+) => {
+	const data: { user: string; text: string } = rawMove;
+	const transformedMove = testAndTransformMove(data.text);
+	if (!transformedMove) {
+		return;
+	}
+	const move: Move = {
+		user: data.user || 'unknown',
+		move: transformedMove,
+		count: 1
+	};
+	setMoves((prev) => {
+		const existingMove = prev.find((m) => m.move === move.move);
+		if (existingMove) {
+			existingMove.count++;
+			return prev;
+		}
+		return [...prev, move];
+	});
+};
