fb376da735fa97626276e34a9108a3f62daca0ea
Anton Siligan <anton.siligan@neinja.dev>
2025-10-11 13:40:22 +0200

    keep old data

--- src/hooks/useEvaluation.ts
+++ src/hooks/useEvaluation.ts
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 export const useEvaluation = (fen: string) => {
 	return useQuery({
 		queryKey: ['evaluation', fen],
-		queryFn: () => getEvaluation(fen)
+		queryFn: () => getEvaluation(fen),
+		placeholderData: (previousData) => previousData
 	});
 };
