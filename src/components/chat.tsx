d2f40a9214fc4f6a89205b73b475a9a3caecd826
Anton Siligan <anton.siligan@neinja.dev>
2025-10-12 08:28:14 +0200

    improve chat headers

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -19,17 +19,23 @@ export const Chat = ({ moves, info, color, clearMove, activeTurn, timeLeft, defa
 
 	return (
 		<div className='flex flex-col gap-4 h-full overflow-hidden'>
-			<h1 className='text-lg font-semibold shrink-0 flex items-center justify-between gap-2'>
-				<div className={cn('flex items-center gap-2', info.platform === 'twitch' ? 'text-purple-500' : 'text-red-500')}>
-					{info.platform === 'twitch' ? <SiTwitch /> : <SiYoutube />} {info.channel}
+			<h1 className='text-lg font-semibold shrink-0 flex items-center gap-2 min-w-0'>
+				<div
+					className={cn(
+						'flex items-center gap-2 min-w-0 flex-1',
+						info.platform === 'twitch' ? 'text-purple-500' : 'text-red-500'
+					)}
+				>
+					{info.platform === 'twitch' ? <SiTwitch className='shrink-0' /> : <SiYoutube className='shrink-0' />}
+					<span className='truncate min-w-0'>{info.channel}</span>
 				</div>
 				<div
 					className={cn(
-						'flex items-center gap-2 border border-white rounded-md py-0.5 uppercase text-sm px-2',
+						'flex items-center gap-2 shrink-0 border border-white rounded-md py-0.5 uppercase text-sm px-2',
 						color === 'white' ? 'text-black bg-white' : 'text-white bg-black'
 					)}
 				>
-					{color}
+					{color.charAt(0).toUpperCase()}
 				</div>
 			</h1>
 			<div className='flex flex-col gap-2 overflow-y-auto flex-1 scroll-smooth'>
