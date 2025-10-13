dd69848c2dbf637d870e0f943f7273c121489472
Anton Siligan <anton.siligan@neinja.dev>
2025-10-13 17:31:47 +0200

    disable strict mode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	// Disable React Strict Mode so effects don't run twice in development
+	reactStrictMode: false
 };
 
 export default nextConfig;
